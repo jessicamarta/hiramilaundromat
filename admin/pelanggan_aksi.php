ea770457aaa46099a9a8bae02fe6d2bd35eef9cf
jessicamarta <jessicamartags@gmail.com>
2025-07-24 11:59:56 +0700

    terbaru versi 2 pelanggan aksi

--- admin/pelanggan_aksi.php
+++ admin/pelanggan_aksi.php
@@ -1,11 +1,3 @@
-<?php
-include '../koneksi.php';
-$nama = $_POST['nama'];
-$hp = $_POST['hp'];
-$alamat = $_POST['alamat'];
-mysqli_query($koneksi,"insert into pelanggan values('','$nama','$hp','$alamat')");
-header("location:pelanggan.php");
-?>
 
 <?php
 include '../koneksi.php';
