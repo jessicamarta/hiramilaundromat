c4e359e100fb7479b1cf4de28c242836866b9c3d
jessicamarta <jessicamartags@gmail.com>
2025-07-24 13:42:19 +0700

    transaksi aksi terbaru

--- admin/transaksi_aksi.php
+++ admin/transaksi_aksi.php
@@ -8,13 +8,13 @@ $status = 0;
 $h = mysqli_query($koneksi,"select harga_per_kilo from harga");
 $harga_per_kilo = mysqli_fetch_assoc($h);
 $harga = $berat * $harga_per_kilo['harga_per_kilo'];
-mysqli_query($koneksi,"insert into transaksi values('','$tgl_hari_ini','$pelanggan','$harga','$berat','$tgl_selesai','$status')");
+mysqli_query($koneksi,"insert into transaksi (transaksi_tgl, transaksi_pelanggan, transaksi_harga, transaksi_berat, transaksi_tgl_selesai, transaksi_status) values('$tgl_hari_ini','$pelanggan','$harga','$berat','$tgl_selesai','$status')");
 $id_terakhir = mysqli_insert_id($koneksi);
 $jenis_pakaian = $_POST['jenis_pakaian'];
 $jumlah_pakaian = $_POST['jumlah_pakaian'];
 for($x=0;$x<count($jenis_pakaian);$x++){
     if($jenis_pakaian[$x] != ""){
-        mysqli_query($koneksi,"insert into pakaian values('','$id_terakhir','$jenis_pakaian[$x]','$jumlah_pakaian[$x]')");
+        mysqli_query($koneksi,"insert into pakaian (pakaian_transaksi, pakaian_jenis, pakaian_jumlah) values('$id_terakhir','$jenis_pakaian[$x]','$jumlah_pakaian[$x]')");
     }
 }
 header("location:transaksi.php");
